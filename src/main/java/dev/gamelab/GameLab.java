74b8a52379e849fa2bca6b5667ada427ceecf8f6
Andrea Rossi <83245043+AndreaRossi03@users.noreply.github.com>
2021-04-29 20:05:09 +0200

    Aggiunto logo

--- src/main/java/dev/gamelab/GameLab.java
+++ src/main/java/dev/gamelab/GameLab.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -58,6 +59,7 @@ public class GameLab extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/scene/Stile.css").toExternalForm());
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/immagini/logo.png")));
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
